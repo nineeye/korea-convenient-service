c561156b7851688e6b7bbb850584744f488c2d6e
nineeye <nineeye@nate.com>
2026-06-22 12:33:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import streamlit as st
 from webtools.pdf_merge import merge_pdfs
 from webtools.pdf_word import convert_pdf_to_word
 from webtools.pdf_image import convert_pdf_to_image
-from webtools.pdf_security import manage_pdf_security  # 추가!
+from webtools.pdf_security import manage_pdf_security  
+from webtools.pdf_compress import compress_pdf
 
 st.sidebar.title("🛠️ PDF 도구 모음")
 # 4번 메뉴 추가
-menu = ["PDF 병합", "PDF → Word 변환", "PDF → 이미지 변환", "PDF 암호 설정/해제"]
+menu = ["PDF 병합", "PDF → Word 변환", "PDF → 이미지 변환", "PDF 암호 설정/해제", "PDF 용량 최적화"]
 choice = st.sidebar.selectbox("기능을 선택하세요", menu)
 
 if choice == "PDF 병합":
@@ -17,3 +18,5 @@ elif choice == "PDF → 이미지 변환":
     convert_pdf_to_image()
 elif choice == "PDF 암호 설정/해제": # 추가!
     manage_pdf_security()
+elif choice == "PDF 용량 최적화":
+    compress_pdf()
