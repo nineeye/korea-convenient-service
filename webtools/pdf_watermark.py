d3333b45d691b90103e1d09ec1122b47628884ba
nineeye <nineeye@nate.com>
2026-06-23 15:45:38 +0900

    Update watermark title from '2단계' to '5단계'

--- webtools/pdf_watermark.py
+++ webtools/pdf_watermark.py
@@ -4,6 +4,11 @@ import io
 from pypdf import PdfReader, PdfWriter
 
 from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
+from PIL import Image
+from reportlab.lib.utils import ImageReader
 
 def add_watermark():
-    st.title("2단계 성공")
+    st.title("5단계 성공")
