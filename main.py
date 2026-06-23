8ebc58c563c3fb0c98779865b2d69bfedbe7dbee
nineeye <nineeye@nate.com>
2026-06-23 11:52:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ from webtools.pdf_remove import remove_pages
 from webtools.pdf_excel import convert_pdf_to_excel
 from webtools.pdf_split import split_pdf
 from webtools.pdf_ocr import pdf_ocr 
+from webtools.pdf_watermark import add_watermark
 
 st.sidebar.title("🛠️ PDF 도구 모음")
-# 4번 메뉴 추가
+
 menu = [
     "PDF 병합",
     "PDF → Word 변환",
@@ -19,11 +20,13 @@ menu = [
     "PDF → Excel 변환",
     "PDF OCR",
     "PDF 분할",
+    "PDF 워터마크 추가",
     "PDF 암호 설정/해제",
     "PDF 용량 최적화",
     "PDF 페이지 회전",
     "PDF 특정 페이지 삭제"
 ]
+
 choice = st.sidebar.selectbox("기능을 선택하세요", menu)
 
 if choice == "PDF 병합":
@@ -37,7 +40,9 @@ elif choice == "PDF → Excel 변환":
 elif choice == "PDF OCR":
     pdf_ocr()
 elif choice == "PDF 분할":
-    split_pdf()        
+    split_pdf()   
+elif choice == "PDF 워터마크 추가":
+    add_watermark()
 elif choice == "PDF 암호 설정/해제": # 추가!
     manage_pdf_security()
 elif choice == "PDF 용량 최적화":
