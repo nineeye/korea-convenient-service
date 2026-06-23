34f5dac3d64b3e9cea9ac6f967d9de23bac46a55
nineeye <nineeye@nate.com>
2026-06-23 12:10:28 +0900

    Add support for custom Korean font in watermark

--- webtools/pdf_watermark.py
+++ webtools/pdf_watermark.py
@@ -4,32 +4,38 @@ import io
 from pypdf import PdfReader, PdfWriter
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
 
 
 def create_watermark(text):
 
     packet = io.BytesIO()
 
-    c = canvas.Canvas(packet, pagesize=letter)
+    pdfmetrics.registerFont(
+        TTFont("Nanum", "fonts/NanumGothic.ttf")
+    )
 
-    c.setFont("Helvetica", 40)
+    c = canvas.Canvas(packet)
 
-    c.saveState()
+    # 연한 회색
+    c.setFillGray(0.75)
 
-    c.translate(300, 400)
+    # 한글 폰트
+    c.setFont("Nanum", 12)
 
-    c.rotate(45)
-
-    c.drawCentredString(0, 0, text)
-
-    c.restoreState()
+    # 하단 우측에 출력
+    c.drawRightString(
+        560,
+        20,
+        text
+    )
 
     c.save()
 
     packet.seek(0)
 
     return PdfReader(packet)
-
 
 def add_watermark():
 
@@ -42,6 +48,16 @@ def add_watermark():
 
     if uploaded_file is None:
         return
+
+    position = st.selectbox(
+    "워터마크 위치",
+    [
+        "하단 우측",
+        "상단 우측",
+        "하단 좌측",
+        "상단 좌측"
+    ]
+)
 
     watermark_text = st.text_input(
         "워터마크 문구",
