078a0f1c2323449f9ed7dd32186089e813aeaed8
nineeye <nineeye@nate.com>
2026-06-27 15:15:31 +0900

    Refactor app.run to use multi-line arguments

--- SellerOS/run.py
+++ SellerOS/run.py
@@ -1,7 +1,10 @@
 from seller.web.app import app
+import os
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 10000))
 
-    app.run(host="0.0.0.0", port=port)
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
